9229dd6ccc11b1a8a0b58a62bc20296028b8a3b8
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-09-06 13:02:52 -0400

    Added routes for home page and register page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// NPM modules
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+// Local imports
+import Header from "./components/Header";
+import Register from "./pages/Register";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    loggedIn: false
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        {!this.state.loggedIn ? (
+          <Route
+            path="/"
+            render={() => (
+              <div className="logged-out">
+                <h2>Please login or signup to view your account</h2>
+                <Button variant="contained" color="primary" className="btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button variant="contained" color="secondary" className="btn">
+                  <Link to="/signup">Signup</Link>
+                </Button>
+              </div>
+            )}
+          />
+        ) : (
+          <Route
+            path="/"
+            render={() => (
+              <div className="logged-in">
+                <h2>Welcome back -username here-</h2>
+              </div>
+            )}
+          />
+        )}
+        <Route exact path="/signup" component={Register} />
       </div>
     );
   }
